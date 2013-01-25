2ee2887a9d69b501aa32edb1281f8a4aa9409ab9
Eric Sorenson <eric.sorenson@puppetlabs.com>
2013-01-25 12:03:15 -0800

    Updated a sshd.rb with the final batch of gotchas that killed this project.

--- eric0_startup/manifests/sshd.rb
+++ eric0_startup/manifests/sshd.rb
@@ -6,19 +6,25 @@
 hostclass 'eric0_startup::sshd' do
 
   # this uses the comma separated argument style
-
-  # service {'sshd': ensure => running, enable => true }
-  service "sshd",
-    :ensure => :running,
-    :enable => :true,
-    :subscribe => File['/etc/ssh/sshd_config']
-
-  package "openssh-server",
-    :ensure => :latest,
-    :notify => Service["sshd"]
-
   file '/etc/ssh/sshd_config',
     :ensure => :present,
     :content => template('eric0_startup/sshd_config.erb')
+
+  # the dependent resource needs to be a quoted string
+  # otherwise it gets immediately searched and fails.
+  package "openssh-server",
+    :ensure => :latest,
+    :notify => "Service[sshd]"
+
+  # likewise, without quoting File, the DSL attempts
+  # execute File[] ...
+  service "sshd",
+    :ensure => :running,
+    :enable => :true,
+    :subscribe => "File[/etc/ssh/sshd_config]"
+
+  # this will actually run Kernel.exec on the master
+  exec "/do/not/bin/echo 'hi there'",
+    :provider => :shell
 
 end
